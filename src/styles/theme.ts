05449fd5ec60eb1b2fa4da8dc667f3b2e0bee2b5
EoBryanDev <mauriciobryan1998@gmail.com>
2023-01-15 13:28:52 -0300

    Initial Project

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  name: 'defaultTheme',
+  author: 'Bryan',
+  projectStyle: {
+    colors: {
+        primaryColor: '',
+        secondaryColor: '',
+    },
+    fontSize: {
+        title : '2rem',
+        subtitle: '1.5rem',
+        body: '1rem'
+    }
+  },
+}
+
+export default theme
