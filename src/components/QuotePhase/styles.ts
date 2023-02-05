4b0aa380d0d8dfc1ff469b29c9d99f25d89d4fbc
EoBryanDev <mauriciobryan1998@gmail.com>
2023-02-05 19:38:25 -0300

    Go back Bugtton Scratching is Working

--- src/components/QuotePhase/styles.ts
+++ src/components/QuotePhase/styles.ts
@@ -2,17 +2,22 @@ import styled, { css } from 'styled-components'
 
 export const Container = styled.section`
   ${({ theme }) => css`
-    background-color: ${theme.projectStyle.colors.primaryColor};
-    width: 600px;
-    height: 300px;
+    background-color: ${theme.projectStyle.colors.secondaryColor};
+    width: 100%
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: 10px;
+    padding: 20px;
   `}
 `
 export const QuoteContainer = styled.article`
-  width: 300px;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   margin-bottom: 10px;
   border-radius: 10px;
   text-align: center;
@@ -20,7 +25,7 @@ export const QuoteContainer = styled.article`
 export const Title = styled.h2`
   ${({ theme }) => css`
     margin-bottom: 10px;
-    color: ${theme.projectStyle.colors.secondaryColor};
+    color: ${theme.projectStyle.colors.primaryColor};
   `}
 `
 export const Author = styled.h4`
--- src/pages/AuthorDetails/index.tsx
+++ src/pages/AuthorDetails/index.tsx
@@ -1,9 +1,15 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 const AuthorDetails = () => {
   const { author } = useParams()
+  const navigate = useNavigate()
 
-  return <h1>{author}</h1>
+  return (
+    <div>
+      <h1>{author}</h1>
+      <button onClick={() => navigate(-1)}>go back</button>
+    </div>
+  )
 }
 
 export default AuthorDetails
